dc7b16bd66d6a85a03eb5d15e6c8cff8a3f887f2
AS-Andro <amoula.selmi1@yahoo.fr>
2024-11-06 20:26:42 +0100

    fix test

--- app/src/main/java/com/tictactoe/mytest/domain/GetWinnerUseCase.kt
+++ app/src/main/java/com/tictactoe/mytest/domain/GetWinnerUseCase.kt
@@ -8,15 +8,18 @@ class GetWinnerUseCase @Inject constructor() {
     suspend operator fun invoke(params: Params): Winner {
         for (i in 0 until 3) {
             when {
-                params.list[i * 3] == Status.O && params.list[(i * 3) + 1] == Status.O && params.list[(i * 3) + 2] == Status.O -> {
-                    return Winner.PLAYER2
+                params.list[i * 3] == Status.X && params.list[(i * 3) + 1] == Status.X && params.list[(i * 3) + 2] == Status.X ||
+                        params.list[i] == Status.X && params.list[i + 3] == Status.X && params.list[i + 6] == Status.X -> {
+                    return Winner.PLAYER1
                 }
 
-                params.list[i * 3] == Status.X && params.list[(i * 3) + 1] == Status.X && params.list[(i * 3) + 2] == Status.X -> {
-                    return Winner.PLAYER1
+                params.list[i * 3] == Status.O && params.list[(i * 3) + 1] == Status.O && params.list[(i * 3) + 2] == Status.O ||
+                        params.list[i] == Status.O && params.list[i + 3] == Status.O && params.list[i + 6] == Status.O -> {
+                    return Winner.PLAYER2
                 }
             }
         }
+
         return Winner.NONE
     }
 
